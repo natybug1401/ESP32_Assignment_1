a5dd6f6a714561d9e734fdd280a9bd582c8372d6
natybug1401 <burgessnathanc@gmail.com>
2023-02-16 12:51:16 +0000

    Update embedded1.ino
    Add comments to help document code

--- embedded1/embedded1.ino
+++ embedded1/embedded1.ino
@@ -6,11 +6,11 @@
       d=3500uS
       mode=2
   */
-  const int pulseWidth = 200;          //parameter A - width of inital pulse (μS) 
-  const int pulseWidthIncrement = 50;  //incremental increase of pulse width (μS)
-  const int pulseSpacing = 2100;       //parameter B - width of space between pulses (μS)
-  const int pulsesPerBlock = 21;       //parameter C - number of pulses in a block
-  const int blockSpacing = 3500;       //parameter D - width of space between blocks (μS)
+  const int pulseWidth = 200;                                 //parameter A - width of inital pulse (μS) 
+  const int pulseWidthIncrement = 50;                         //incremental increase of pulse width (μS)
+  const int pulseSpacing = 2100;                              //parameter B - width of space between pulses (μS)
+  const int pulsesPerBlock = 21;                              //parameter C - number of pulses in a block
+  const int blockSpacing = 3500;                              //parameter D - width of space between blocks (μS)
   
   //define constants for pin numbers
   const int switch1Pin = 5;
@@ -20,7 +20,6 @@
 
 
 void setup() {
-
   //set the pins for the buttons as inputs and for the signals as outputs
   pinMode(switch1Pin, INPUT);
   pinMode(switch2Pin, INPUT);
@@ -29,36 +28,39 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(switch1Pin) == HIGH) {
-    sigB;
+  if (digitalRead(switch1Pin) == HIGH) {                      //if switch 1 is pressed
+    sigB;                                                     //run signal B
+
+    if (digitalRead(switch2Pin) == HIGH){sigAMod;}            //if switch 2 is also pressed, run the modified version of signal A
+    else {sigA;}                                              //if switch 2 is not pressed, run the regular version of signal A
+
+    delayMicroseconds(blockSpacing);                          //wait for a time defined by parameter D, before looping to start a new block of pulses
     
-    if (digitalRead(switch2Pin) == HIGH){sigAInverted;}
-    else {sigA;}
-    
-    delayMicroseconds(blockSpacing);
   }
 }
 
+//method to generate signal A
 void sigA(){
-  for (uint8_t i=0; i<pulsesPerBlock; i++){
-    digitalWrite(signalAPin, HIGH);
-    delayMicroseconds(pulseWidth+(pulseWidthIncrement*i));
-    digitalWrite(signalAPin, LOW);
-    delayMicroseconds(pulseSpacing);
+  for (uint8_t i=0; i<pulsesPerBlock; i++){                   //iterate from 0 up to Parameter C
+    digitalWrite(signalAPin, HIGH);                           //set the signal high
+    delayMicroseconds(pulseWidth+(pulseWidthIncrement*i));    //wait for a time defined by parameter A + parameter B multiplied by the iteration value, as iteration counts up, this will increase the pulse length each iteration
+    digitalWrite(signalAPin, LOW);                            //set the signal low
+    delayMicroseconds(pulseSpacing);                          //wait for a time defined by parameter B
   }
 }
 
-void sigAInverted(){
-  for (uint8_t i=pulsesPerBlock; i>0; i--){
-    digitalWrite(signalAPin, HIGH);
-    delayMicroseconds(pulseWidth+(pulseWidthIncrement*i));
-    digitalWrite(signalAPin, LOW);
-    delayMicroseconds(pulseSpacing);
+//method to generate the modified signal A
+void sigAMod(){
+  for (uint8_t i=pulsesPerBlock; i>0; i--){                   //iterate from Parameter C down to 0 
+    digitalWrite(signalAPin, HIGH);                           //set the signal high
+    delayMicroseconds(pulseWidth+(pulseWidthIncrement*i));    //wait for a time defined by parameter A + parameter B multiplied by the iteration value, as iteration counts down, this will decrease the pulse length each iteration
+    digitalWrite(signalAPin, LOW);                            //set the signal low
+    delayMicroseconds(pulseSpacing);                          //wait for a time defined by parameter B
   }
 }
 
 void sigB(){
-  digitalWrite(signalBPin, HIGH);
-  delayMicroseconds(50);
-  digitalWrite(signalBPin, LOW);
+  digitalWrite(signalBPin, HIGH);                             //set the signal high
+  delayMicroseconds(50);                                      //wait 50 microseconds
+  digitalWrite(signalBPin, LOW);                              //set the signal low
 }
